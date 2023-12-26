dcb5965550954ae5f1a87137f48b73065e1d9d3a
aka7774 <aka7774@users.noreply.github.com>
2023-12-26 22:05:40 +0900

    Fix llamasrv

--- install/llamasrv.sh
+++ install/llamasrv.sh
@@ -6,5 +6,7 @@ cd llamasrv
 python3 -m venv venv
 curl -kL https://bootstrap.pypa.io/get-pip.py | venv/bin/python
 
-venv/bin/activate
+source venv/bin/activate
 FORCE_CMAKE=1 CMAKE_ARGS="-DLLAMA_CUBLAS=on" pip install llama-cpp-python --force-reinstall --upgrade --no-cache-dir
+
+venv/bin/python -m pip install uvicorn anyio starlette fastapi sse_starlette starlette_context
--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ if [ ! -e venv ]; then
   bash setup/onegai.sh
 fi
 
-venv/bin/python -m gunicorn -w 4 -t 0 -b 0.0.0.0:58083 -k uvicorn.workers.UvicornWorker main:app
+venv/bin/python -m gunicorn -w 4 -t 0 -b 0.0.0.0:$1 -k uvicorn.workers.UvicornWorker main:app
