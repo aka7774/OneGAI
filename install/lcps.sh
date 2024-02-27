60dc11f0646ad1f8802788143de14108af893019
aka7774 <aka7774@users.noreply.github.com>
2024-02-27 21:34:58 +0900

    Updte lcps

--- install/lcps.sh
+++ install/lcps.sh
@@ -6,6 +6,7 @@ cd lcps
 python3 -m venv venv
 curl -kL https://bootstrap.pypa.io/get-pip.py | venv/bin/python
 
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" venv/bin/python -m pip install llama-cpp-python --upgrade --force-reinstall --no-cache-dir
 venv/bin/python -m pip install 'llama-cpp-python[server]'
 
 # wget https://huggingface.co/TheBloke/calm2-7B-chat-GGUF/resolve/main/calm2-7b-chat.Q4_K_M.gguf
