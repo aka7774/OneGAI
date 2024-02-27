e88faa6e504b5f52b583517d6c9805eb39e04038
aka7774 <aka7774@users.noreply.github.com>
2024-02-27 20:27:11 +0900

    Add lcps

---
+++ install/lcps.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+mkdir lcps
+cd lcps
+
+python3 -m venv venv
+curl -kL https://bootstrap.pypa.io/get-pip.py | venv/bin/python
+
+venv/bin/python -m pip install 'llama-cpp-python[server]'
+
+# wget https://huggingface.co/TheBloke/calm2-7B-chat-GGUF/resolve/main/calm2-7b-chat.Q4_K_M.gguf
+# venv/bin/python -m llama_cpp.server --model calm2-7b-chat.Q4_K_M.gguf --host 0.0.0.0 --port 50000 --n_gpu_layers 81
