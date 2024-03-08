8dfab9c21fae3d586ceef6d36e77d3961b8b70c7
aka7774 <aka7774@users.noreply.github.com>
2024-03-09 02:15:54 +0900

    Add lsof

--- tab/control.py
+++ tab/control.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from tab.refresh import create_refresh_button
 
 import onegai.services
@@ -9,26 +10,33 @@ def get_status():
         st.append(onegai.services.get_status(app_name))
     return "\n".join(st)
 
+def get_lsof_i():
+    cmd = 'lsof -iTCP | grep LISTEN'
+    result = subprocess.run(cmd, shell=True, text=True, stdout=subprocess.PIPE)
+    return result.stdout
+
 def get_installed():
     return os.listdir('apps')
 
 def start(app_name):
     result = onegai.services.start(app_name, False)
-    return result, get_status()
+    return result, get_status(), get_lsof_i()
 
 def stop(app_name):
     result = onegai.services.stop(app_name)
-    return result, get_status()
+    return result, get_status(), get_lsof_i()
 
 def restart(app_name):
     result = onegai.services.start(app_name, True)
-    return result, get_status()
+    return result, get_status(), get_lsof_i()
 
 def gr_tab(gr):
     with gr.Tab('control'):
         info = gr.Textbox(label='', interactive=False)
         status = gr.Textbox(value=get_status(), label='server status', interactive=False)
         create_refresh_button(gr, status, lambda: None, lambda: {'value': get_status()}, 'refresh-button', interactive=True)
+        lsof_i = gr.Textbox(value=get_lsof_i(), label='lsof -iTCP | grep LISTEN', interactive=False)
+        create_refresh_button(gr, lsof_i, lambda: None, lambda: {'value': get_lsof_i()}, 'refresh-button', interactive=True)
         with gr.Row():
             installed = gr.Dropdown(choices=get_installed(), label='installed')
             create_refresh_button(gr, installed, lambda: None, lambda: {'choices': get_installed()}, 'refresh-button', interactive=True)
@@ -40,17 +48,17 @@ def gr_tab(gr):
     start_button.click(
         fn=start,
         inputs=[installed],
-        outputs=[info, status],
+        outputs=[info, status, lsof_i],
     )
 
     stop_button.click(
         fn=stop,
         inputs=[installed],
-        outputs=[info, status],
+        outputs=[info, status, lsof_i],
     )
 
     restart_button.click(
         fn=restart,
         inputs=[installed],
-        outputs=[info, status],
+        outputs=[info, status, lsof_i],
     )
